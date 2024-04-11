07b19a15ba454abce114b490788477058eb085fc
Hugo Lindell <98535571+hulind@users.noreply.github.com>
2024-04-11 17:04:42 +0200

    Update git-push-all.sh

--- git-push-all.sh
+++ git-push-all.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Get current branch
+current_branch=$(git branch --show-current)
+
+# Find CPAY number
+if [[ $current_branch =~ /CPAY-([0-9]+) ]]; then
+  number=${BASH_REMATCH[1]}
+  echo "Found CPAY-$number"
+else
+  echo "Branch name does not contain the pattern /CPAY-XXX"
+  exit 1
+fi
+
 # Check if a commit message is provided as an argument
 if [ $# -eq 0 ]; then
   echo "Usage: $0 <commit_message>"
@@ -13,7 +25,7 @@ commit_message="$*"
 git add .
 
 # Commit changes with the provided message
-git commit -m "$commit_message"
+git commit -m "CPAY-$number $commit_message"
 
 # Push the changes to the remote repository
 git push
